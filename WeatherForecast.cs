315cf5771ecbf114c1b975c5735271d56a9c3361
Nicolas <n.abuin2005@gmail.com>
2024-12-15 13:04:34 +0100

    .gitignore creado, estructura webapi creada

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace back_1_trimestre_2_daw_solitario;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
